3d6e74f4fe9e16eeaceecff0f7589f6313c59e2b
sini-george <sini.george.9529@gmail.com>
2024-01-11 16:23:35 +0000

    ADR-748 changed Other to OtherFermentedProduct

--- it/uk/gov/hmrc/alcoholdutycalculator/RatesIntegrationSpec.scala
+++ it/uk/gov/hmrc/alcoholdutycalculator/RatesIntegrationSpec.scala
@@ -20,7 +20,7 @@ import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import uk.gov.hmrc.alcoholdutycalculator.base.ISpecBase
 import uk.gov.hmrc.alcoholdutycalculator.controllers.routes
-import uk.gov.hmrc.alcoholdutycalculator.models.AlcoholRegime.{Beer, Other, Wine}
+import uk.gov.hmrc.alcoholdutycalculator.models.AlcoholRegime.{Beer, OtherFermentedProduct, Wine}
 import uk.gov.hmrc.alcoholdutycalculator.models.RateType.Core
 import uk.gov.hmrc.alcoholdutycalculator.models.{AlcoholRegime, RateBand, RatePeriod, RateType}
 
@@ -36,7 +36,7 @@ class RatesIntegrationSpec extends ISpecBase {
         s"?ratePeriod=${Json.toJson(YearMonth.of(2023, 5))(RatePeriod.yearMonthFormat).toString()}&rateType=${Json
           .toJson[RateType](Core)
           .toString}&abv=${Json.toJson(3).toString}&alcoholRegimes=${Json
-          .toJson(Set(Json.toJson[AlcoholRegime](Beer), Json.toJson[AlcoholRegime](Wine), Json.toJson[AlcoholRegime](Other)))
+          .toJson(Set(Json.toJson[AlcoholRegime](Beer), Json.toJson[AlcoholRegime](Wine), Json.toJson[AlcoholRegime](OtherFermentedProduct)))
           .toString()}"
 
       lazy val result =
