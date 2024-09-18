f5f9feeb79b33e3a507da9b254d11f8d77bd7f7f
sini-george <sini.george.9529@gmail.com>
2024-09-18 09:24:20 +0100

    ADR-1181 library updates, http verb changes, pr bot warning fixes

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 import uk.gov.hmrc.DefaultBuildSettings
 
 ThisBuild / majorVersion := 0
-ThisBuild / scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "2.13.14"
 
 lazy val microservice = Project("alcohol-duty-calculator", file("."))
   .enablePlugins(play.sbt.PlayScala, SbtDistributablesPlugin)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,20 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion    = "9.4.0"
+  private val mockitoScalaVersion = "1.17.37"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "com.beachape" %% "enumeratum" % "1.7.3",
-    "com.beachape" %% "enumeratum-play" % "1.8.0"
+    "uk.gov.hmrc"  %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "com.beachape" %% "enumeratum"                % "1.7.4",
+    "com.beachape" %% "enumeratum-play"           % "1.8.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % bootstrapVersion % Test,
-    "org.mockito"       %% "mockito-scala"            % "1.17.30"        % Test,
-    "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"    % Test,
-    "org.mockito"       %% "mockito-scala-scalatest"  % "1.17.30"        % Test
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % bootstrapVersion    % Test,
+    "org.mockito"       %% "mockito-scala"            % mockitoScalaVersion % Test,
+    "org.mockito"       %% "mockito-scala-scalatest"  % mockitoScalaVersion % Test,
+    "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"       % Test
   )
 
   val itDependencies = Seq.empty
