e03cae34c31f2b03ba91b4ec9a90cc131c09c2e3
ajchilts <40767309+ajchilts@users.noreply.github.com>
2024-11-29 16:29:54 +0000

    Initial commit POC of validation for rates file

--- it/test/uk/gov/hmrc/alcoholdutycalculator/RatesIntegrationSpec.scala
+++ it/test/uk/gov/hmrc/alcoholdutycalculator/RatesIntegrationSpec.scala
@@ -16,15 +16,51 @@
 
 package uk.gov.hmrc.alcoholdutycalculator
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.fge.jackson.JsonLoader
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.main.{JsonSchema, JsonSchemaFactory}
 import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import uk.gov.hmrc.alcoholdutycalculator.base.ISpecBase
 import uk.gov.hmrc.alcoholdutycalculator.controllers.routes
 import uk.gov.hmrc.alcoholdutycalculator.models.{RateBand, RatePeriod, RateType}
 
+import java.io.InputStream
 import java.time.YearMonth
 
 class RatesIntegrationSpec extends ISpecBase {
+
+  "validates rate file against schema" in {
+    val stream: InputStream = getClass.getResourceAsStream("/rates/alcohol-duty-rates.json")
+    val lines               = scala.io.Source.fromInputStream(stream).getLines().mkString
+    validateJsonAgainstSchema(lines) mustBe true
+  }
+
+  "for beer" - {
+    "where tax type code 311, 312, 313, 314, 315" - {
+      "service rates endpoint must" - {
+        "show the correct rate" in {
+          stubAuthorised()
+
+          val expectedRate: Double = 9.27d
+
+          val urlParams =
+            s"?ratePeriod=${Json.toJson(YearMonth.of(2023, 5))(RatePeriod.yearMonthFormat).toString()}&alcoholRegimes=Beer"
+
+          lazy val result =
+            callRoute(FakeRequest("GET", routes.RatesController.rates().url + urlParams))
+
+          status(result) mustBe OK
+          val rateBandList = Json.parse(contentAsString(result)).as[Seq[RateBand]]
+
+          rateBandList.find(x => Seq("311", "312", "313", "314", "315").contains(x.taxTypeCode)).get.rate mustBe Some(
+            expectedRate
+          )
+        }
+      }
+    }
+  }
 
   "service rates endpoint must" - {
     "respond with 200 status" in {
@@ -54,5 +90,15 @@ class RatesIntegrationSpec extends ISpecBase {
       val rateBand: RateBand = Json.parse(contentAsString(result)).as[RateBand]
       rateBand.rateType mustBe RateType.Core
     }
+  }
+
+  private lazy val jsonSchemaFactory: JsonSchemaFactory = JsonSchemaFactory.byDefault
+  private lazy val jsonSchema: JsonSchema               =
+    jsonSchemaFactory.getJsonSchema(s"resource:/rates/alcohol-duty-rates-schema.json")
+
+  def validateJsonAgainstSchema(inputDoc: String): Boolean = {
+    val inputJson: JsonNode      = JsonLoader.fromString(inputDoc)
+    val report: ProcessingReport = jsonSchema.validate(inputJson)
+    report.isSuccess
   }
 }
