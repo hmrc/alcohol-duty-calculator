8cdd0d8adf06388958dbdd0f0e2b96c36e8664e7
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-05-21 21:58:22 +0100

    DDCE-8147 - ADR: Add a test script

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core"                 % "2.13.0",
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "com.beachape"  %% "enumeratum"                % "1.9.5",
-    "com.beachape"  %% "enumeratum-play"           % "1.9.5"
+    "com.beachape"  %% "enumeratum"                % "1.9.7",
+    "com.beachape"  %% "enumeratum-play"           % "1.9.7"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatestplus"          %% "scalacheck-1-17"        % "3.2.18.0"       % Test,
     ("com.github.java-json-tools" %  "json-schema-validator"  % "2.2.14").exclude("org.mozilla", "rhino"),
-    "org.mozilla" % "rhino" % "1.7.15.1"
+    "org.mozilla" % "rhino" % "1.9.1"
   )
 
   val itDependencies: Seq[Nothing] = Seq.empty
