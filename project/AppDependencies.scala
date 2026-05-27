98c3ae50ae598ab9b6ffcc86cff4e834e675ab33
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-05-27 09:51:02 +0100

    DDCE-8147 - Run scalafmtAll

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion    = "10.7.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core"                 % "2.13.0",
@@ -14,8 +14,8 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatestplus"          %% "scalacheck-1-17"        % "3.2.18.0"       % Test,
-    ("com.github.java-json-tools" %  "json-schema-validator"  % "2.2.14").exclude("org.mozilla", "rhino"),
-    "org.mozilla" % "rhino" % "1.9.1"
+    ("com.github.java-json-tools" % "json-schema-validator"  % "2.2.14").exclude("org.mozilla", "rhino"),
+    "org.mozilla"                 % "rhino"                  % "1.9.1"
   )
 
   val itDependencies: Seq[Nothing] = Seq.empty
